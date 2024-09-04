7af3f0ae7d966c1370ec1b3a7a654b8e7cd58a1f
theaashiq <mdaashiqin2000@gmail.com>
2024-09-05 03:01:23 +0530

    updates

--- src/components/pages/mainpage/main/footer.jsx
+++ src/components/pages/mainpage/main/footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import './footer.css'
 
 const Footer = () => {
@@ -13,13 +14,22 @@ const Footer = () => {
         <div className='footer-BlockB'>
           <div>&copy; 2023-2024</div>
           <div className='footer-ComName'>Elega Leather</div>
-          <div>mdaashiqin2000@gmail.com</div>
+          <div onClick={() => window.location.href = 'mailto:mdaashiqin2000@gmail.com'}>
+            mdaashiqin2000@gmail.com
+          </div>
         </div>
         <div className='footer-BlockC'>
           <div style={{marginBottom:'8px'}}>Follow us</div>
           <div>
-            <InstagramIcon/>
-            <LinkedInIcon style={{marginLeft:"5px"}}/>
+            <GitHubIcon
+              style={{cursor:'pointer'}}
+              onClick={() => window.open('https://github.com/theaashiq', '_blank')}/>
+            <LinkedInIcon
+              style={{margin: '0px 14px', cursor:'pointer'}}
+              onClick={() => window.open('https://www.linkedin.com/in/mohammed-aashiq-445569247/', '_blank')}/>
+            <InstagramIcon
+              style={{cursor:'pointer'}}
+              onClick={() => window.open('https://www.instagram.com/_the_aashiq?igsh=MW5zejlpbXZxcDgxdg==', '_blank')}/>
           </div>
         </div>
       </div>
